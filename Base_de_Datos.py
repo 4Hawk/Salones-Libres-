72e69928a0ff3c8beca1875a9d5dd0664e449519
Shamil Rivera Acacio <reyiv19@gmail.com>
2018-10-23 17:31:42 -0500

    Agregando Base de Datos de Usuarios

---
+++ Base_de_Datos.py
@@ -0,0 +1,16 @@
+
+# Base de Datos para Identificación del Usuario
+ 
+datos1 = {1811854:"JavierIn34",17035465:"MariaRojas123",16042341:"Peru123",
+13094354:"Usilperu"}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Identificación_del_Usuario.py
@@ -0,0 +1,17 @@
+                            # Preguntar al Usuario - Codigo y Contraseña
+from Base_de_Datos import datos1
+
+# Verificando Usuario
+codigo = int(input(" Ingrese su código de Estudiante-----> "))
+validar = codigo in datos1
+while(validar==False):
+    codigo = int(input(" Ingrese su código de Estudiante------> "))
+    validar = codigo in datos1
+else:
+    obtain = datos1.get(codigo)
+    contraseña = input(" Ingrese su contraseña------> ") 
+    while(obtain!=contraseña):
+        contraseña = input(" Ingrese su contraseña-------> ") 
+    else:
+        print(" Bienvenido(a)")
+
