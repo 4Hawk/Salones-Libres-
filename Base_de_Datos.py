67b5a3c5eed6b7c44ea7bb968e295586c1ebd634
Shamil Rivera <shamil.rivera@usil.pe>
2018-10-24 07:57:14 -0500

    Agregando Datos a la Base de Datos1

--- Base_de_Datos.py
+++ Base_de_Datos.py
@@ -2,4 +2,4 @@
 # Base de Datos para Identificación del Usuario
  
 datos1 = {1811854:"JavierIn34",17035465:"MariaRojas123",16042341:"Peru123",
-13094354:"Usilperu"}
+13094354:"Usilperu",15051243:"easy",17114354:"password",11112222:"estudiante"}
