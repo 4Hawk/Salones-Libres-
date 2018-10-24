7fd2469119d5204335236243e38d6326555839ac
jquispeh <juanpabloquispeh@gmail.com>
2018-10-24 08:51:22 -0500

    se añadio datos para el campus

--- Base_de_Datos.py
+++ Base_de_Datos.py
@@ -2,7 +2,7 @@
 # Base de Datos para Identificación del Usuario
  
 datos1 = {1811854:"JavierIn34",17035465:"MariaRojas123",16042341:"Peru123",
-13094354:"Usilperu",15051243:"easy",17114354:"password",11112222:"estudiante"}
+13094354:"Usiperu",15051243:"easy",17114354:"password",11112222:"estudiante"}
 
 # Base de Datos de Campus - Pabellon - Aula
 # C1 = A - B - C - D  Pabellones
