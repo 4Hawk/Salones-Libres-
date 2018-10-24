ec10bb5ad5291aa3defb91b74d83460183333018
jquispeh <juanpabloquispeh@gmail.com>
2018-10-24 08:50:45 -0500

    se añadio datos para los campus

--- Base_de_Datos.py
+++ Base_de_Datos.py
@@ -3,3 +3,12 @@
  
 datos1 = {1811854:"JavierIn34",17035465:"MariaRojas123",16042341:"Peru123",
 13094354:"Usilperu",15051243:"easy",17114354:"password",11112222:"estudiante"}
+
+# Base de Datos de Campus - Pabellon - Aula
+# C1 = A - B - C - D  Pabellones
+# C2 = A - B - C      Pabellones
+# C3 = A - B -        Pabellones
+
+datosC1 = [101,102,103,104,105,106,107]
+datosC2 = []
+datosC7 = []
